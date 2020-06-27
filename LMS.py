c23097ca2f1d92e99ad23c7a10cb3ed286135a4c
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-06-27 22:31:49 +0530

    Update LMS.py

--- LMS.py
+++ LMS.py
@@ -1,11 +1,13 @@
-                      # LIBRARY MANAGMENT SYSTEM
+                    # LIBRARY MANAGMENT SYSTEM
 import time
+import random
+
 
 class library:
-    secret_key = "1234"              # to get admin access
-    delkey = "001"                   # to get permission for deletion
+    secret_key = random.randint(1000, 9999)             # to get admin access
+    delkey = random.randint(100, 999)                   # to get permission for deletion
 
-    def __init__(self, libraryName, allBooks, listOfBooks ):
+    def __init__(self, libraryName, allBooks, listOfBooks):
         self.lendbooklis = {}
         self.allBooks = allBooks
         self.list_of_books = listOfBooks
@@ -57,25 +59,51 @@ class library:
                     print("Invalid key!")
             count -= 1
 
-
-# Set library name & books in library (innitially)
-
-# libname = input("Enter your library name : ")
-# initai_stock=int(input("Enter number of book you're putting in library : "))
-# initial_book_list = []
-# print("enter books")
-# for item in range(initai_stock):
-#     bookstock = input()
-#     initial_book_list.append(bookstock)
-#
-# xyzlib = library(libname, initial_book_list, initial_book_list)
-xyzlib = library("xlibrary12", ["book1", "book2", "book3","book4"], ["book1", "book2", "book3","book4"])
+    def changeKey(self):
+        key = int(input("Enter secret key : "))
+        count = 2
+        while count>0:
+            if key == self.secret_key:
+                while True:
+                    keyChange = input("\t1 --> Change Secret Key\n\t2 --> Change deletion key")
+                    if keyChange == "1":
+                        while True:
+                            new_secretKey1 = int(input("Enter new Secret key : "))
+                            new_secretKey2 = int(input("Re-enter new Secret key : "))
+                            if new_secretKey1 == new_secretKey2:
+                                self.secret_key = new_secretKey1
+                                break
+                            else:
+                                print("key didn't match..!")
+                                continue
+                    elif keyChange == "2":
+                        while True:
+                            new_delKey1 = int(input("Enter new Deletion key : "))
+                            new_delKey2 = int(input("Re-enter new Deletion key : "))
+                            if new_delKey1 == new_delKey2:
+                                self.delkey = new_delKey1
+                                break
+                            else:
+                                print("key didn't match..!")
+                                continue
+                    elif keyChange == "0":
+                        print("exit...")
+                        time.sleep(3)
+                        break
+                    else:
+                        print('INVALID OPTION..! Try again')
+                        continue
+            else:
+                print('INVALID KEY..!')
+                count -= 1
+                continue
 
 
 # admin() method, for Admin functions (includes all functions)
 def admin():
     print("\tDisplay all available books --> 1\tLend book --> 2\n\tDisplay lended books --> 3\t\t\tAdd new book --> 4"
-          "\n\tReturn a book --> 5\t\t\t\t\tRemove book --> 6\n\tDisplay All books --> 7\t\t\t\tExit --> 0")
+          "\n\tReturn a book --> 5\t\t\t\t\tRemove book --> 6\n\tDisplay All books --> 7"
+          "\t\t\t\tChange Keys --> 8\n\tExit --> 0")
     choice = int(input("Enter your choice : "))
     Exit = False
     try:
@@ -99,6 +127,8 @@ def admin():
                 xlib.removeBook()
             elif choice == 7:
                 xlib.displayAllBooks()
+            elif choice == 8:
+                xlib.changeKey()
             else:
                 print("Invalid choice")
             choice = int(input("Enter your choice : "))
@@ -108,8 +138,8 @@ def admin():
 
 # userfun() method, for user functions (allows display, lend & return books)
 def userfun():
-    print("  Display all available books --> 1\t\t\tLend book --> 2\n\t\t    Display all books --> 3\t"
-          "\tReturn a book --> 4\nExit --> 0")
+    print("  Display all available books --> 1\t\t\tLend book --> 2\n\t\t    Display all books --> 4\t"
+          "\tReturn a book --> 3\nExit --> 0")
     choice = int(input("Enter your choice : "))
     Exit = False
     while Exit is not True:
@@ -129,9 +159,40 @@ def userfun():
             print("Invalid choice")
         choice = int(input("Enter your choice : "))
 
+
+exit = False
+while exit is not True:
+    libname = input("Enter your library name : ")
+    initai_stock = int(input("Enter number of book you're putting in library : "))
+    initial_book_list = []
+    print("enter books")
+    for item in range(initai_stock):
+        bookstock = input()
+        initial_book_list.append(bookstock)
+    ALLBOOKS = initial_book_list[:]
+    # library.secret_key = random.randint(1000, 9999)
+    # library.delkey = random.randint(100, 999)
+    xlib = library(libname, ALLBOOKS, initial_book_list)
+
+    print(f"\tYour secret key = {library.secret_key}\n\t Your deletion key = {library.delkey}\n")
+
+    all_set = input("We're all set, press any key to start\t\t\tpress 0 to make changes or setup again\n"
+                    "\t\t\t\t\t\t\t\t\t\t ")
+    if all_set == "0":
+        continue
+    else:
+        break
+# xlib = library(libname, allBooks, initial_book_list)
+
+# xlib = library("xlibrary12", ["book1", "book2", "book3","book4"], ["book1", "book2", "book3","book4"])
+
+
 lib2 = library.secret_key              # secret key object
 
-if __name__ == '__main__':                # main() function
+
+
+if __name__ == '__main__':                # main function
+
     localtime = time.asctime(time.localtime(time.time()))
     print(f"{localtime}\t\t\t\t\t\t\tExit --> 0")
     Exit = False
@@ -140,7 +201,7 @@ if __name__ == '__main__':                # main() function
         kc=2
         if user == "y":
             while(kc > 0):
-                adkey = input("Enter admin key : ")
+                adkey = int(input("Enter admin key : "))
                 if adkey == lib2:
                     print("////////////WELCOME SIR!/////////////")
                     print(admin())
@@ -149,7 +210,7 @@ if __name__ == '__main__':                # main() function
                     print("Invalid key!")
                 kc -= 1
         elif user == "n":
-            print(f"/////////////////////Welcome to {xyzlib.libraryName}//////////////////////// ")
+            print(f"/////////////////////Welcome to {xlib.libraryName}//////////////////////// ")
             print(userfun())
         elif user == "0":
             Exit = True
